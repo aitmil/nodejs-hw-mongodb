054068b7acddeb1dfe19065d000483f0b4dd6a2d
Anna Taranchuk <taranchuk4@yahoo.com>
2024-07-25 19:26:24 +0300

    step 5/6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { initMongoDB } from "./db/initMongoConnection.js";
 import { setupServer } from "./server.js";
 
 const bootstrap = async () => {
-  await initMongoDB;
+  await initMongoDB();
   setupServer();
 };
 
