783a631219856c4b31042db0592beb4ad1d64ee1
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-02 00:29:05 +0300

    fix bug

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -9,15 +9,6 @@ export const errorHandler = (err, req, res, next) => {
       data: err.message,
     });
   }
-
-  if (err instanceof mongoose.Error.CastError) {
-    return res.status(400).json({
-      status: 400,
-      message: 'Invalid ID format',
-      data: err.message,
-    });
-  }
-
   res.status(500).json({
     status: 500,
     message: 'Something went wrong',
