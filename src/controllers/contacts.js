2dff75502d90fef080963e06da3966eb5a944f73
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-06 19:55:50 +0300

    step 3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,8 +1,11 @@
 import createHttpError from 'http-errors';
 import * as ContactService from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getAllContacts = async (req, res) => {
-  const contacts = await ContactService.getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+
+  const contacts = await ContactService.getAllContacts({ page, perPage });
 
   res.status(200).json({
     status: 200,
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,23 @@
 import { ContactsCollection } from '../models/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = () => {
-  return ContactsCollection.find();
+export const getAllContacts = async ({ page, perPage }) => {
+  const limit = perPage;
+  const skip = page > 0 ? (page - 1) * perPage : 0;
+
+  const contactsQuery = ContactsCollection.find();
+
+  const [contacts, count] = await Promise.all([
+    contactsQuery.skip(skip).limit(limit).exec(),
+    ContactsCollection.countDocuments(),
+  ]);
+
+  const paginationData = calculatePaginationData(count, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = (contactId) => {
